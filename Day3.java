d9ef2906fad49a421749a0c2f63ca3eef823b825
Adam Fox <adam@foxy.id.au>
2024-12-04 18:29:26 +1030

    Day 3 part 2

--- Day3.java
+++ Day3.java
@@ -8,9 +8,9 @@ import java.util.regex.Matcher;
 public class Day3 {
     public static void main(String[] args) {
         var part1 = new Part1();
-        // var part2 = new Part2();
-        System.out.println("Part 1: " + part1.run("day2_input.txt"));
-        // System.out.println("Part 2: " + part2.run("day2_input.txt"));
+        var part2 = new Part2();
+        System.out.println("Part 1: " + part1.run("day3_test_input.txt"));
+        System.out.println("Part 2: " + part2.run("day3_input.txt"));
 
     }
 }
@@ -30,7 +30,7 @@ class FileProcessor {
 
 class Part1 {
     public int run(String filename) {
-        var fileProcessor = new FileProcessor("day3_input.txt");
+        var fileProcessor = new FileProcessor(filename);
         var pattern = Pattern.compile("mul\\((\\d+),(\\d+)\\)");
         int result = 0;
 
@@ -45,6 +45,47 @@ class Part1 {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return result;
+    }
+}
+
+class Part2 {
+    public long run(String filename) {
+        var fileProcessor = new FileProcessor(filename);
+        var mulPattern = Pattern.compile("mul\\((\\d+),(\\d+)\\)");
+        var dontPattern = Pattern.compile("(.*?)don't\\(\\).*?do\\(\\)(.*)", Pattern.DOTALL);
+        var dontOnlyPattern = Pattern.compile("(.*?)don't\\(\\).*", Pattern.DOTALL);
+        long result = 0;
+        String realInstructions = "";
+        String instructions;
+        Matcher matcher;
+
+        try {
+            instructions = fileProcessor.getString();
+            while (instructions != "") {
+                matcher = dontPattern.matcher(instructions);
+                if (matcher.find()) {
+                    realInstructions += matcher.group(1);
+                    instructions = matcher.group(2);
+                } else {
+                    matcher = dontOnlyPattern.matcher(instructions);
+                    if (matcher.find()) {
+                        realInstructions += matcher.group(1);
+                    } else {
+                        realInstructions += instructions;
+                    }
+                    instructions = "";
+                }
+            }
+
+            matcher = mulPattern.matcher(realInstructions);
+            while (matcher.find()) {
+                result += Integer.parseInt(matcher.group(1)) * Integer.parseInt(matcher.group(2));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         return result;
     }
 }
