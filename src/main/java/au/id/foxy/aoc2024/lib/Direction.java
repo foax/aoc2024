cca666451ad4fbd45e7d6547ce1fc15a774438f7
Adam Fox <adam@foxy.id.au>
2024-12-16 09:43:03 +1030

    Refactor WordSearch to use Grid class

--- src/main/java/au/id/foxy/aoc2024/lib/Direction.java
+++ src/main/java/au/id/foxy/aoc2024/lib/Direction.java
@@ -1,11 +1,12 @@
 package au.id.foxy.aoc2024.lib;
 
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class Direction {
-    private static final Map<String, Vector> DIRECTIONS = Map.of(
+    private static final Map<String, Vector> DIRECTION_TO_VECTOR_MAP = Map.of(
         "N", new Vector(new Coord(0, -1), 1),
         "NE", new Vector(new Coord(1, -1), 1),
         "E", new Vector(new Coord(1, 0), 1),
@@ -14,11 +15,12 @@ public class Direction {
         "SW", new Vector(new Coord(-1, 1), 1),
         "W", new Vector(new Coord(-1, 0), 1),
         "NW", new Vector(new Coord(-1, -1), 1));
+    private static final List<String> DIRECTIONS = List.of("N", "NE", "E", "SE", "S", "SW", "W", "NW");
     private final String direction;
 
     public Direction(String dir) {
-        if (!DIRECTIONS.containsKey(dir)) {
-            throw new java.lang.IllegalArgumentException("dir must be one of %s".formatted(DIRECTIONS.keySet().stream().collect(Collectors.joining(", "))));
+        if (!DIRECTION_TO_VECTOR_MAP.containsKey(dir)) {
+            throw new java.lang.IllegalArgumentException("dir must be one of %s".formatted(DIRECTION_TO_VECTOR_MAP.keySet().stream().collect(Collectors.joining(", "))));
         }
         this.direction = dir;
     }
@@ -28,7 +30,15 @@ public class Direction {
     }
 
     public Vector getDirAsVector() {
-        return DIRECTIONS.get(this.direction);
+        return DIRECTION_TO_VECTOR_MAP.get(this.direction);
+    }
+
+    public Direction next(boolean diagonals, boolean clockwise) {
+        int pos = DIRECTIONS.indexOf(this.direction);
+        int increment = diagonals ? 1 : 2;
+        if (!clockwise)
+            increment *= -1;
+        return new Direction(DIRECTIONS.get((pos + increment) % DIRECTIONS.size()));
     }
 
     @Override
